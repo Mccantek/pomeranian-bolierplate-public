28b17f3728a9743abbb959e1b37d9027a7651505
Mccantek <michal.cantek@gmail.com>
2023-06-07 20:47:17 +0200

    menu changes

--- src/App/Layout/AppAside.jsx
+++ src/App/Layout/AppAside.jsx
@@ -1,6 +1,13 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import './styles/aside.css';
+import { HouseIcon } from '../Components/Icons/HouseIcon';
+import { EditIcon } from '../Components/Icons/EditIcon';
+import { SettingIcon } from '../Components/Icons/SettingIcon';
+import { PersonalCardIcon } from '../Components/Icons/PersonalCardIcon';
+import { ElementIcon } from '../Components/Icons/ElementIcon';
+import { CalendarIcon } from '../Components/Icons/CalendarIcon';
+// import { Link } from 'react-router-dom';
 
 export function AppAside() {
   return (
@@ -8,13 +15,44 @@ export function AppAside() {
       <nav>
         <ul>
           <li>
-            <NavLink to="dashboard">Dashboard</NavLink>
+            <NavLink className="aside-row" to="dashboard">
+              <HouseIcon className="menu-icon" />
+              Dashboard
+            </NavLink>
           </li>
           <li>
-            <NavLink to="blocks">Bloki</NavLink>
+            <NavLink className="aside-row" to="My CV">
+              {' '}
+              <PersonalCardIcon className="menu-icon" />
+              Moje cv
+            </NavLink>
           </li>
           <li>
-            <NavLink to="exercises">Ćwiczenia</NavLink>
+            <NavLink className="aside-row" to="blocks">
+              {' '}
+              <ElementIcon className="menu-icon" />
+              Bloki
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="aside-row" to="exercises">
+              {' '}
+              <EditIcon className="menu-icon" />
+              Ćwiczenia
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="aside-row" to="Calendar">
+              {' '}
+              <CalendarIcon className="menu-icon" />
+              Kalendarz
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className="aside-row" to="settings">
+              <SettingIcon className="menu-icon" />
+              Ustawienia
+            </NavLink>
           </li>
         </ul>
       </nav>
