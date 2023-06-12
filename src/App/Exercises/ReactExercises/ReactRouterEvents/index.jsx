5e4d7ec8274515dd1eb1159000cb1c806af66897
Mccantek <michal.cantek@gmail.com>
2023-06-12 21:10:37 +0200

    added some events

--- src/App/Exercises/ReactExercises/ReactRouterEvents/index.jsx
+++ src/App/Exercises/ReactExercises/ReactRouterEvents/index.jsx
@@ -1,20 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './style.css';
 
 export function ReactRouterEvents() {
+  const [getCount, setCount] = useState(0);
+
+  function handleClick(event) {
+    return console.log('Jestem klikniętym buttonem');
+  }
+
+  function handleOnChangeInput(event) {
+    const inputValue = event.target.value;
+    return console.log('Wprowadzona wartość: ', inputValue);
+  }
+
+  function handleIncrement() {
+    setCount(getCount + 1);
+  }
+
+  function handleDecrement() {
+    setCount(getCount - 1);
+  }
+
   return (
-    <div className="form--container">
-      <h1>Cześć!</h1>
-      <label for="textInput" className='label'>Wpisz tekst</label>
-      <input
-        type="text"
-        id="textInput"
-        name="textInput"
-        placeholder="tekst do wpisania"
-      />
+    <>
+      <div className="form--container">
+        <h1>Cześć!</h1>
+        <label for="textInput" className="label">
+          Wpisz tekst
+        </label>
+        <input
+          type="text"
+          id="textInput"
+          name="textInput"
+          placeholder="tekst do wpisania"
+          onChange={handleOnChangeInput}
+        />
+        <br />
+        <input
+          onClick={handleClick}
+          type="button"
+          value="KLIKNIJ"
+          className="click"
+        />
+      </div>
       <br />
-      <input type="button" value="KLIKNIJ" className="click" />
-    </div>
+      <br />
+      <div className="counter--container">
+        <p>Licznik: {getCount}</p>
+        <input
+          onClick={handleIncrement}
+          type="button"
+          value={
+            getCount === 0
+              ? `Kliknięto we mnie ${getCount} razy`
+              : `Super kliknięto we mnie wiele razy - ${getCount}`
+          }
+        />
+        <input onClick={handleDecrement} type="button" value="Decrement" />
+      </div>
+    </>
   );
 }
