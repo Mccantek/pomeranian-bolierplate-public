7a165079b432292410fc6dc328733fffb2002fe5
Mccantek <michal.cantek@gmail.com>
2023-06-16 18:23:22 +0200

    google font task

---
+++ src/App/Exercises/HtmlCss/GoogleFonts/router-data.jsx
@@ -0,0 +1,11 @@
+import { GoogleFont } from '.';
+import React from 'react';
+
+export const GoogleFontMeta = {
+  path: 'google-font',
+  date: '16-06-2023',
+  linkLabel: 'import',
+  blockNo: 11,
+  element: <GoogleFont />,
+  tags: ['text', 'fundaments'],
+};
---
+++ src/App/Exercises/HtmlCss/TextFundaments/TextFundaments.jsx
@@ -0,0 +1,6 @@
+import './styles.css';
+import React from 'react';
+
+export function TextFundaments() {
+  return <div></div>;
+}
