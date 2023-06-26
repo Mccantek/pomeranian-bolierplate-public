262314a1f3dc38029b256ad20d9397b414c6e639
Mccantek <michal.cantek@gmail.com>
2023-06-26 17:26:23 +0200

    to merge all

--- src/App/Layout/AppHeader.jsx
+++ src/App/Layout/AppHeader.jsx
@@ -3,15 +3,41 @@ import React from 'react';
 import './styles/header.css';
 import { Logo } from '../Components/Logo/Logo';
 import { Link } from 'react-router-dom';
+import Vector from '../Images/Vector.svg';
+import test from '../Images/test.svg';
+import cogwheel from '../Images/setting.svg';
 
 export function AppHeader() {
   return (
     <header>
-      <Link to="/">
-        <Logo />
-      </Link>
-      <div>
-        <div className="header-title">Tytuł nagłówka</div>
+      {/* <img src={Logo} className="logo" alt="" /> */}
+      <div className="left">
+        <Link to="/">
+          <img src={test} alt="" srcSet="" />
+        </Link>
+      </div>
+      <div className="right">
+        <button
+          className="settings"
+          onClick={() => {
+            console.log('Clicked cogwheel');
+          }}
+        >
+          {' '}
+          <img src={cogwheel} alt="" className="cogwheel" />{' '}
+        </button>
+        <div className="profile-picture">
+          <div className="placeholder"></div>
+        </div>
+        <div className="user-details">
+          <p>Michał</p>
+          <p>kursant</p>
+        </div>
+        <button className='dropdown-menu' onClick={() => {
+            console.log('Clicked setting');
+          }}>
+          <img src={Vector} className="arrow" alt="" />
+        </button>
       </div>
     </header>
   );
