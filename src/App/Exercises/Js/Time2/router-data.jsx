c0b3f2f66d337e491a9fec90a2e0bde25fcdfd5c
Mccantek <michal.cantek@gmail.com>
2023-07-03 21:15:31 +0200

    zabawa z czasem

---
+++ src/App/Exercises/Js/Time2/router-data.jsx
@@ -0,0 +1,15 @@
+import Time2 from './index';
+
+export const blockRouterMetaData = {
+  path: 'js-czas-funkcje2',
+
+  date: '12-12-2023',
+
+  linkLabel: 'CzasFunkcje2',
+
+  blockNo: 1,
+
+  element: <Time2 />,
+
+  tags: ['components'],
+};
