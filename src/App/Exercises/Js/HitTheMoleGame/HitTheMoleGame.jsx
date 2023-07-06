453e0b65af38cb450cde74b44125ad1c357ff7c3
Mccantek <michal.cantek@gmail.com>
2023-07-06 21:26:23 +0200

    random mole

--- src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/HitTheMoleGame.jsx
@@ -7,27 +7,33 @@ import MoleGameBoard from './MoleGameBoard';
 export function HitTheMoleGame() {
   const defaultGameTime = 2 * 60 * 1000;
   const [gameTime, setGameTime] = useState(defaultGameTime / 1000);
-  const [seconds, setSeconds] = useState(gameTime / 1000);
+  // const [seconds, setSeconds] = useState(gameTime / 1000);
 
   const [moleCount, setMoleCount] = useState(1);
   const [scoreCount, setScoreCount] = useState(0);
-
+  const [score, setScore] = useState(0);
   const [moleArray, setMoleArray] = useState(
     Array(10).fill({ isVisible: false, isWhacked: false })
   );
-  // const [countdown, setCountdown] = useState(initialCountdownState);
 
-  useEffect(() => {
-    let intervalId;
-
-    if (!intervalId) {
-      intervalId = setInterval(() => {
-        setSeconds(seconds - 1);
+  const CountdownTimer = ({ initialCountdownState }) => {
+    const [countdown, setCountdown] = useState(initialCountdownState);
+    useEffect(() => {
+      if (!countdown) return;
+      const interval = setInterval(() => {
+        setCountdown((prevCountdown) => prevCountdown - 1000);
       }, 1000);
-    }
-    console.log(seconds.toString().padStart(2, 0));
-    return () => clearInterval(intervalId);
-  }, [seconds]);
+
+      return () => {
+        clearInterval(interval);
+      };
+    }, [countdown]);
+    return (
+      <div>
+        <p>time remaining: {countdown / 1000} seconds</p>
+      </div>
+    );
+  };
 
   function hitTheMole(index) {
     console.log(moleArray[index].isWhacked);
@@ -37,6 +43,25 @@ export function HitTheMoleGame() {
     moleArray[index].isWhacked = !moleArray[index].isWhacked;
 
     console.log(moleArray[index].isWhacked);
+  }
+
+  function showRandomMole() {
+    function getRandomIntInclusive(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    const random = getRandomIntInclusive(0, moleArray.length - 1);
+
+    setMoleArray((previousMoleArray) =>
+      previousMoleArray.map((mole, index) => {
+        const updatedMole = { ...mole };
+        updatedMole.isVisible = index === random;
+
+        return updatedMole;
+      })
+    );
   }
 
   return (
@@ -53,6 +78,9 @@ export function HitTheMoleGame() {
         moleArray={moleArray}
         hitTheMole={hitTheMole}
       />
+
+      <CountdownTimer initialCountdownState={gameTime} />
+      <button onClick={() => showRandomMole()}>HELLO THERE</button>
     </>
   );
 }
--- src/App/Exercises/Js/HitTheMoleGame/MoleGameBoard.jsx
+++ src/App/Exercises/Js/HitTheMoleGame/MoleGameBoard.jsx
@@ -2,18 +2,29 @@ import React from 'react';
 import Mole from './mole.svg';
 const MoleGameBoard = ({ scoreCount, moleArray, hitTheMole }) => {
   return (
-
-    <div className="moleGame">
-        <h4>Current score = {scoreCount} </h4>
-      {moleArray.map((mole, index) => {
-        <div>
-          <span>
-            {mole.isVisible ? (
-              <img src={Mole} onClick={() => hitTheMole(index)} />
-            ) : null}
-          </span>
-        </div>;
-      })}
+    <div className="moleGame" >
+      <h4>Current score = {scoreCount} </h4>
+      <div style={{ display: 'flex', gap: '5px' }}>
+        {moleArray.map((mole, index) => {
+          return (
+            <div
+              style={{
+                width: '104px',
+                height: '104px',
+                backgroundColor: '#EBEBEB',
+                borderRadius: '8px',
+                margin: '5px',
+              }}
+            >
+              <span>
+                {mole.isVisible ? (
+                  <img src={Mole} onClick={() => hitTheMole(index)} />
+                ) : null}
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
