24a615b3c936ff521976c1f306aadf10edee7577
Mccantek <michal.cantek@gmail.com>
2023-07-09 12:23:31 +0200

    fixed cookies!

--- src/App/Components/Cookies/Cookies.jsx
+++ src/App/Components/Cookies/Cookies.jsx
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import cookies from './cookie.svg';
 
 const Cookies = () => {
-  const handleAgreement = () => {};
-  const handleAgreementCustomization = () => {};
+  const [agreed, setAgreed] = useState(false);
+
+  const handleAgreement = () => {
+    setAgreed(true);
+  };
+  const handleAgreementCustomization = () => {
+    setAgreed(true);
+  };
+
+  if (agreed) {
+    return null;
+  }
+
   return (
     <div className="cookies-agreement ">
-      <div className='left'>
-      <div className="text-wrapper">
-        {' '}
-        <img className="cookies-icon" src={cookies} alt="cookies icon" />
-        <p>
+      <div className="left">
+        <div className="text-wrapper">
           {' '}
-          Pozwól na pliki cookies Nasza strona korzysta z ciasteczek, które
-          umożliwiają poprawne działanie strony i pomagają nam świadczyć usługi
-          na najwyższym poziomie. Możesz zaakceptować wykorzystanie przez nas
-          wszystkich tych plików i przejść do strony lub dostosować użycie do
-          swoich referencji. W tym celu kliknij przycisk po prawej stronie
-          “dopasuj zgody”, aby następnie wybrać te które odpowiadają Twoim
-          indywidualnym preferencjom.{' '}
-        </p>{' '}
-      </div></div>
+          <img className="cookies-icon" src={cookies} alt="cookies icon" />
+          <p>
+            Pozwól na pliki cookies Nasza strona korzysta z ciasteczek, które
+            umożliwiają poprawne działanie strony i pomagają nam świadczyć
+            usługi na najwyższym poziomie. Możesz zaakceptować wykorzystanie
+            przez nas wszystkich tych plików i przejść do strony lub dostosować
+            użycie do swoich referencji. W tym celu kliknij przycisk po prawej
+            stronie “dopasuj zgody”, aby następnie wybrać te które odpowiadają
+            Twoim indywidualnym preferencjom.
+          </p>{' '}
+        </div>
+      </div>
       <div className="button-wrapper">
         {' '}
         <button className="agree" onClick={handleAgreement}>
